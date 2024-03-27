dc3b75e1cbaa4cd8fe439697a1f79d19c5035b05
Mheyrie <omota4fidelity@gmail.com>
2024-03-27 21:26:12 +0100

    Updated Update link and Save link

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -37,9 +37,11 @@ cart.forEach((cartItem) => {
             <span>
               Quantity: <span class="quantity-label">${cartItem.quantity}</span>
             </span>
-            <span class="update-quantity-link link-primary">
+            <span class="update-quantity-link link-primary js-update-link" data-product-id="${matchingProduct.id}">
               Update
             </span>
+            <input class="quantity-input">
+            <span class="save-quantity-link link-primary js-save-link">Save</span>
             <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
               Delete
             </span>
@@ -98,12 +100,12 @@ cart.forEach((cartItem) => {
 document.querySelector('.js-order-summary')
   .innerHTML = cartSummaryHTML;
 
+  // Handles deleting item from the cart
 document.querySelectorAll('.js-delete-link')
   .forEach((link) => {
     link.addEventListener('click', () => {
       const productId = link.dataset.productId;
       removeFromCart(productId);
-
       const container = document.querySelector(
         `.js-cart-item-container-${productId}`
       );
@@ -114,8 +116,17 @@ document.querySelectorAll('.js-delete-link')
 
 function updateCartQuantity(){
   const cartQuantity = calculateCartQuantity()
-
   document.querySelector('.js-return-to-home-link').innerHTML = `${cartQuantity} items`
-  
 }
 updateCartQuantity()
+
+
+document.querySelectorAll('.js-update-link')
+  .forEach((link) => {
+      link.addEventListener('click', ()=>{
+        const productId = link.dataset.productId
+
+        const container = document.querySelector(`.js-cart-item-container-${productId}`);
+        container.classList.add('is-editing-quantity')
+      })
+  });
