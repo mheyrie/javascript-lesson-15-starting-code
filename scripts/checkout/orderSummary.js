712ff234c306ef84492eedd5e46b3990212e6f9e
Mheyrie <omota4fidelity@gmail.com>
2024-04-27 03:43:11 +0100

    Made payment summary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -3,6 +3,7 @@ import {products, getProduct} from '../../data/products.js';
 import {formatCurrency} from '../utils/money.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js'
 import {deliveryOptions,getDeliveryOption} from '../../data/deliveryOptions.js' 
+import { renderPaymentSummary } from "./paymentSummary.js";
 
 
 export function renderOrderSummary(){
@@ -123,6 +124,8 @@ export function renderOrderSummary(){
       link.addEventListener('click', () => {
         const productId = link.dataset.productId;
         removeFromCart(productId);
+        renderPaymentSummary()
+
         const container = document.querySelector(
           `.js-cart-item-container-${productId}`
         );
@@ -166,6 +169,7 @@ export function renderOrderSummary(){
 
       
       updateQuantity(productId, newQuantity)
+      renderPaymentSummary()
 
       const container = document.querySelector(
         `.js-cart-item-container-${productId}`
@@ -195,6 +199,7 @@ export function renderOrderSummary(){
         const {productId, deliveryOptionId} = element.dataset
         updateDeliveryOption(productId, deliveryOptionId)
         renderOrderSummary()
+        renderPaymentSummary()
       })
     })
 }
