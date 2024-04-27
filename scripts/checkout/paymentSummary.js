d60ac5f024157caa57c68e78a92294753cec551b
Mheyrie <omota4fidelity@gmail.com>
2024-04-27 03:37:18 +0100

    Save data and generated HTML for payment summary

--- scripts/checkout/paymentSummary.js
+++ scripts/checkout/paymentSummary.js
@@ -1,6 +1,7 @@
 import { cart } from "../../data/cart.js";
 import { getProduct } from "../../data/products.js";
 import { getDeliveryOption } from "../../data/deliveryOptions.js";
+import { formatCurrency } from "../utils/money.js";
 
 
 export function renderPaymentSummary(){
@@ -28,31 +29,33 @@ export function renderPaymentSummary(){
 
         <div class="payment-summary-row">
             <div>Items (3):</div>
-            <div class="payment-summary-money">$42.75</div>
+            <div class="payment-summary-money">$${formatCurrency(productPriceCents)}</div>
         </div>
 
         <div class="payment-summary-row">
             <div>Shipping &amp; handling:</div>
-            <div class="payment-summary-money">$4.99</div>
+            <div class="payment-summary-money">$${formatCurrency(shippingPriceCent)}</div>
         </div>
 
         <div class="payment-summary-row subtotal-row">
             <div>Total before tax:</div>
-            <div class="payment-summary-money">$47.74</div>
+            <div class="payment-summary-money">$${formatCurrency(totalBeforeTaxCent)}</div>
         </div>
 
         <div class="payment-summary-row">
             <div>Estimated tax (10%):</div>
-            <div class="payment-summary-money">$4.77</div>
+            <div class="payment-summary-money">$${formatCurrency(taxCents)}</div>
         </div>
 
         <div class="payment-summary-row total-row">
             <div>Order total:</div>
-            <div class="payment-summary-money">$52.51</div>
+            <div class="payment-summary-money">$${formatCurrency(totalCent)}</div>
         </div>
 
         <button class="place-order-button button-primary">
             Place your order
         </button>
     `
+    document.querySelector('.js-payment-summary')
+        .innerHTML = paymentSummaryHTML
 }
